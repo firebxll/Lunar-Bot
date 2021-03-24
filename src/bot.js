7601a94bb8c7a1b04b1a11d1334409d5bf835002
firebxll <80926201+firebxll@users.noreply.github.com>
2021-03-24 17:22:22 -0400

    Add files via upload

---
+++ src/bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '.';
+
+client.once('ready', () => {
+    console.log('Lunar is online!')
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).split("");
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping') {
+        message.channel.send('pong!');
+    }
+})
+
+client.login("ODIzOTY0NDYwMzI2MzIyMjM4.YFoesw.cezgS1H3X979S5cxMjYIEySSHYg")
