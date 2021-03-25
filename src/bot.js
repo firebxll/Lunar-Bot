0bfa791ae017e97d94446bb75592f6a0b2177bcd
firebxll <80926201+firebxll@users.noreply.github.com>
2021-03-25 18:49:30 -0400

    Added a help command.
    Sends an embed with all working commands that are used for something

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,29 @@
-const Discord = require('discord.js');
+
+ 
+const Discord = require('discord.js'); //sets login stuff up
 const client = new Discord.Client();
-const prefix = "" //set prefix here
+const prefix = "/" //set prefix here and will always work with the const variable
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
+}); //sends the login message
 
 client.on('message', msg => {
   if (msg.content === prefix + 'ping') {
-    msg.reply('pong'); //test command to see if the bot is working
+    msg.reply('pong'); //@'s user and sends pong back
 }
+})
+
+client.on('message', msg =>{
+  if(msg.content === prefix + 'help') { //gets the message with the prefix
+    msg.channel.send( //sends a message or embed using Discord.MessageEmbed to send an embed instead
+      new Discord.MessageEmbed()
+        .setTitle('**Help**') //title of the embed
+        .setDescription('These are all the available commands and what they do.')
+        .setColor('fff000')
+        .addField('**Commands**', 'kick - kicks target member\nban - bans targer member\nsocials - prints selected socials\nhelp - prints this embed') //\n just makes a new line and everything else is self explanatory
+    )
+  } 
 })
 
 client.on('message', msg => { //this is an embed that prints when you run [prefix]socials
@@ -19,35 +33,35 @@ client.on('message', msg => { //this is an embed that prints when you run [prefi
         .setTitle('**Socials**')
         .setDescription('Make sure to follow all of these!')
         .setColor('ff0000')
-        .addField('Socials', '[Twitter](//enter twitter link here)\n[Youtube](//enter youtube like here)', false)
-    )
+        .addField('Socials', '[Twitter]()\n[Youtube]()', false) //put twitter and youtube links here
+    ) //the brackets and shit grab the link and just print whats in the bracket as a hyperlink
   }
 })
 
-client.on('message', message => {
+client.on('message', message => { //this is just a message event
     if (!message.guild) return; //This checks if the message is in a guild
     if (message.content.startsWith(prefix + 'kick')) { //this finds the message
       if(!message.member.hasPermission('KICK_MEMBERS')) { //this checks the permissions of the user
         message.reply('you do not have permission to do that.');
         return}; //this sends a message if the user does not have the required permissions
       const user = message.mentions.users.first()
-      if (user) {
+      if (user) { //this grabs a mention from the message
         const member = message.guild.member(user);
-        if (member) {
+        if (member) { //this is sent if the command works
           member
             .kick('User has been kicked.')
-            .then(() => {
-              message.reply(`successfully kicked ${user.tag}`);
+            .then(() => { //audit log message
+              message.reply(`successfully kicked ${user.tag}`); //sends this in the channel where the command was run and user.tag just grabs the mention of the user
             })
-            .catch(err => {
-              message.reply('I was unable to kick the member');         
+            .catch(err => { //catches a role heirarchy or bot permission error and sends an error message to the channel and console
+              message.reply('I was unable to kick the member');          
               console.error(err);
             });
         } else {       
           message.reply("that user isn't in this guild!");
-        }  
+        }  //if the user is not in the server this is sent
       } else {
-        message.reply("you didn't mention the user to kick!");
+        message.reply("you didn't mention the user to kick!"); //if the user does not have the required permissions this is sent
       }
     }
   })
@@ -82,4 +96,4 @@ client.on('message', message => {
     }
   });
 
-client.login("TOKEN") //put token here
+client.login('') //put token here
